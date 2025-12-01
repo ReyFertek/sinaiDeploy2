c7c4f6bd520f9f5b1cb4e402008ae3932fb6bcfe
ReyFertek <f3rteky@gmail.com>
2025-12-01 13:22:23 -0300

    pagesSpeed Insights vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,15 @@
 import { Analytics } from "@vercel/analytics/next"
+ 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
