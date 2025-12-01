3399f53ca6301bde4c6c707a05efd75bd1718ef6
ReyFertek <f3rteky@gmail.com>
2025-12-01 13:41:11 -0300

    pagesSpeed Insights vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,22 @@
+
+import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+
+export const metadata = {
+  title: "Sinai Pedras",
+  description: "Descrição do site",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
 
 import { SpeedInsights } from '@vercel/speed-insights/next';
 export default function RootLayout({
