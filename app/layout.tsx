c442c4f689fee0d494633ffc0fd29eeb7dacba32
ReyFertek <f3rteky@gmail.com>
2025-12-01 13:25:56 -0300

    pagesSpeed Insights vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import { Analytics } from "@vercel/analytics/next"
- 
-export default function RootLayout({ children }) {
+
+import { SpeedInsights } from '@vercel/speed-insights/next';
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <head>
